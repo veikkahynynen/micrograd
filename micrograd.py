64a302cf827145ed588d81d4954f42444ba16ef8
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-21 14:30:05 +0200

    class Value

--- micrograd.py
+++ micrograd.py
@@ -3,6 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+
+
+class Value:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"Value(data={self.data}"
+
+
+
+
+
 def f(x):
     return 3*x**2 - 4*x + 9
 
