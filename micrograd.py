518894e484a9331313ed2191422d7c464b55f7ae
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-21 15:21:46 +0200

    graphviz

--- micrograd.py
+++ micrograd.py
@@ -1,20 +1,68 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{ data %.4f }" % (n.data,), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
 
 
 
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
-        return f"Value(data={self.data}"
+        return f"Value(data={self.data})"
 
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
 
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
+    def ____(self, other):
+        out = Value(self.data - other.data, (self, other))
+        return out
+
+    def ____(self, other):
+        out = Value(self.data / other.data, (self, other))
+        return out
 
 
 
@@ -22,11 +70,12 @@ def f(x):
     return 3*x**2 - 4*x + 9
 
 
+
 def main():
-    x = 3.0
-    print(f(x))
-    h = 0.001
-    print((f(x + h) - f(x)) / h)
+    a = Value(3.0)
+    dot = draw_dot(a)
+    dot.render('graph', format='png', cleanup=True)
+
 
 
 if __name__ == "__main__":
