d870692e337ffa4309286cb8eb80a43a354e4bb2
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-21 12:05:52 +0200

    f(x)

---
+++ micrograd.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return 3*x**2 - 4*x + 5
+
+
+
+def main():
+    print(f(5.0))
+
+
+
+if __name__ == "__main__":
+    main()
