04ee2e7e62c2cf56355fac6f404457a93b63a380
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-28 09:00:51 +0200

    gradient update

--- micrograd.py
+++ micrograd.py
@@ -78,14 +78,18 @@ def main():
     w2 = Value(1.0, label='w2')
 
     # bias of the neuron
-    b = Value(6.7, label='b')
+    b = Value(6.8813735870195432, label='b')
 
     # h1*w1 + x2*w2 + b
     x1w1 = x1*w1; x1w1.label = 'x1*w1'
     x2w2 = x2*w2; x2w2.label = 'x2*w2'
     x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1 + x2w2'
     n = x1w1x2w2 + b; n.label = 'n'
-    o = n.tanh()
+    o = n.tanh(); o.label = 'o'
+    o.grad = 1.0
+    n.grad = 0.5
+    x1w1x2w2.grad = 0.5
+    b.grad = 0.5
 
     dot = draw_dot(o)
     dot.render('graph', format='png', cleanup=True)
