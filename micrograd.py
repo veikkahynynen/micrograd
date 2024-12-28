317a49a469730ace20aa8cee416f2f66f69656fd
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-28 17:31:56 +0200

    _backward +=

--- micrograd.py
+++ micrograd.py
@@ -56,8 +56,8 @@ class Value:
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
         out._backward = _backward
 
         return out
@@ -66,8 +66,8 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
 
         return out
@@ -78,7 +78,7 @@ class Value:
         out = Value(t, (self, ), 'tanh')
 
         def _backward():
-            self.grad = (1 - t**2) * out.grad
+            self.grad += (1 - t**2) * out.grad
 
         out._backward = _backward
 
