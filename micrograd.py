608ff4d27c6baec7988e1dd86e5638e284ff02f8
veikkahynynen <veikka.hynynen@gmail.com>
2024-12-28 17:51:00 +0200

    clear unnecessary lines

--- micrograd.py
+++ micrograd.py
@@ -108,14 +108,11 @@ def main():
     # inputs x1, x2
     x1 = Value(2.0, label='x1')
     x2 = Value(0.0, label='x2')
-
     # weights w1, w2
     w1 = Value(-3.0, label='w1')
     w2 = Value(1.0, label='w2')
-
     # bias of the neuron
     b = Value(6.8813735870195432, label='b')
-
     # h1*w1 + x2*w2 + b
     x1w1 = x1*w1; x1w1.label = 'x1*w1'
     x2w2 = x2*w2; x2w2.label = 'x2*w2'
