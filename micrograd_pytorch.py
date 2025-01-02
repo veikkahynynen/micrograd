7a37f64e8abf3adf41995ca042f15a89e4d435f7
veikkahynynen <veikka.hynynen@gmail.com>
2025-01-02 12:38:09 +0200

    graph

--- micrograd_pytorch.py
+++ micrograd_pytorch.py
@@ -1,6 +1,41 @@
 import torch
 import random
 import math
+import matplotlib.pyplot as plt
+from graphviz import Digraph
+
+
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
 
 
 class Value:
@@ -142,6 +177,9 @@ def main():
     x = [2.0, 3.0, -1]
     n = MLP(3, [4, 4, 1])
     print(n(x))
+
+    dot = draw_dot(n(x))
+    dot.render('graph', format='png', cleanup=True)
 
 
 if __name__ == "__main__":
