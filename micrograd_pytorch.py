6288211ca0978140fab53b110cd76ffb10f77acd
veikkahynynen <veikka.hynynen@gmail.com>
2025-01-02 12:05:29 +0200

    class MLP

--- micrograd_pytorch.py
+++ micrograd_pytorch.py
@@ -127,6 +127,16 @@ class Layer:
         return outs
 
 
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
 
 
 def main():
